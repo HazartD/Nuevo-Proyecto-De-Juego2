b6db31e4d86a6cc26be000400b0ae4db839006c5
HazartD <120349577+HazartD@users.noreply.github.com>
2023-08-14 14:12:06 -0600

    cambie el padre a un canvas layer para ver si servia pero no

--- menu.gd
+++ menu.gd
@@ -1,20 +1,24 @@
 extends Control
 func _ready():
-	$tutorial.text= tr("menu","es")
-
-func _process(_delta):
-	pass
+	$tutorial.text = tr("menu","es")
+	$jump.text = tr("jump","es")
+	$trap.text = tr("trap","es")
+	$enem.text = tr("enem","es")
+	$destructor.text = tr("destructor","es")
+	
+	
 
 func _input(_event):
 	if  Input.is_action_just_pressed("esc"):
 		self.set_visible(!self.visible)
 		get_tree().set_pause(!get_tree().paused)
-		if "seminknuc" in Powahuptas.powahuptas:$jump.show()
-		else: $jump.hide()
-		if "machacavergas" in Powahuptas.powahuptas:$trap.show()
-		else: $trap.hide()
-		if "tegoelpoderabsolutoymelapelas" in Powahuptas.powahuptas:$enem.show()
-		else: $enem.hide()
-		if "aafceq'aibfue'unoab" in Powahuptas.powahuptas:$destructor.show()
-		else: $destructor.hide()
-		$TextEdit.text= "babosadas:"+ str(Powahuptas.babosadas) + "      Cruz:"+str(Powahuptas.CRUZ)+"      XXX:"+str(Powahuptas.XXX)
+		if self.visible :
+			if "seminknuc" in Powahuptas.powahuptas:$jump.show()
+			else: $jump.hide()
+			if "machacavergas" in Powahuptas.powahuptas:$trap.show()
+			else: $trap.hide()
+			if "tegoelpoderabsolutoymelapelas" in Powahuptas.powahuptas:$enem.show()
+			else: $enem.hide()
+			if "aafceq'aibfue'unoab" in Powahuptas.powahuptas:$destructor.show()
+			else: $destructor.hide()
+			$TextEdit.text= "babosadas:"+ str(Powahuptas.babosadas) + "      Cruz:"+str(Powahuptas.CRUZ)+"      XXX:"+str(Powahuptas.XXX)
